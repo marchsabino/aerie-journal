b58962d67b97ee5dce444558a67329224d492d0e
Marcello Sabino <marchsabino@gmail.com>
2020-06-28 03:04:28 -0400

    Added Footer Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { NewsfeedComponent } from './views/newsfeed/newsfeed.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NewsfeedComponent
+    NewsfeedComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
