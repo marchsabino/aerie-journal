6de051cdc67aedd43f43b5e1b45c32a133f85769
Marcello Sabino <marchsabino@gmail.com>
2020-07-03 19:59:22 -0400

    Styling Improvements for Forums

--- src/app/views/control-panel/forums/board/board.component.spec.ts
+++ src/app/views/control-panel/forums/board/board.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BoardComponent } from './board.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 describe('BoardComponent', () => {
   let component: BoardComponent;
@@ -9,7 +10,7 @@ describe('BoardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, SharedModule],
       declarations: [BoardComponent],
     }).compileComponents();
   }));
